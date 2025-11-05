188da0471f1f677294f896855ef936258ed55c6f
atonzhang <aton_zhang_yd@outlook.com>
2025-11-05 17:18:41 +0800

    support base sepolia network

--- script/HelpConfig.s.sol
+++ script/HelpConfig.s.sol
@@ -12,6 +12,7 @@ abstract contract CodeConstants {
     // CHAIN IDS
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
+    uint256 public constant BASE_SEPOLIA_CHAIN_ID = 84532;
 }
 
 
@@ -28,6 +29,7 @@ contract HelpConfig is CodeConstants, Script {
 
     constructor() {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
+        networkConfigs[BASE_SEPOLIA_CHAIN_ID] = getBaseSepoliaConfig();
     }
 
     function getConfigByChainId(uint256 chainId) public returns(NetworkConfig memory) {
@@ -40,9 +42,15 @@ contract HelpConfig is CodeConstants, Script {
         }
     }
 
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+    function getSepoliaEthConfig() public pure returns(NetworkConfig memory) {
         return NetworkConfig({
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // ETH / USD
+        });
+    }
+
+    function getBaseSepoliaConfig() public pure returns(NetworkConfig memory) {
+        return NetworkConfig({
+            priceFeed: 0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1 // ETH / USD
         });
     }
 
